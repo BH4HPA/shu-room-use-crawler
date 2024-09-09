32cea0bda8d43ae559584b94f70f305ea2befb22
Ray <i@r-ay.cn>
2024-09-09 18:24:39 +0800

    feat: docker image with commit sha label

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t ccr.ccs.tencentyun.com/shuhole/shu-room-use-crawler:latest --platform linux/amd64 .
+docker build -t ccr.ccs.tencentyun.com/shuhole/shu-room-use-crawler:`git rev-parse --short HEAD` --platform linux/amd64 .
 docker login --username=100035268144 ccr.ccs.tencentyun.com
-docker push ccr.ccs.tencentyun.com/shuhole/shu-room-use-crawler:latest
+docker push ccr.ccs.tencentyun.com/shuhole/shu-room-use-crawler:`git rev-parse --short HEAD`
