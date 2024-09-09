56e9e66ac77a362238a8ef890091c7517fc8d43d
Ray <i@r-ay.cn>
2024-09-09 18:29:03 +0800

    ci: 完善 build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo Going to build `git rev-parse --short HEAD` image
 docker build -t ccr.ccs.tencentyun.com/shuhole/shu-room-use-crawler:`git rev-parse --short HEAD` --platform linux/amd64 .
+echo Built `git rev-parse --short HEAD` image, going to push
+echo Logging in
 docker login --username=100035268144 ccr.ccs.tencentyun.com
+echo Pushing
 docker push ccr.ccs.tencentyun.com/shuhole/shu-room-use-crawler:`git rev-parse --short HEAD`
